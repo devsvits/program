d5ec2b237025ef903e544544c99e84fd72e9f5ae
root <root@Nagra.(none)>
2013-01-10 17:00:12 +0530

    fix bug in equal.cpp

--- equal.cpp
+++ equal.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
 	int n1,n2;
-	cout<<"enter first number=";
+	cout<<"enter first number:";
 	cin >> n1;
-	cout<<"enter second number=";
+	cout<<"enter second number:";
 	cin >> n2;
  	if (n1==n2)
 	    cout << "both numbers are equal";
