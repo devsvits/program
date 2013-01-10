4fe2e0c8f58494c92544df04e530846b13c3e297
root <root@localhost.localdomain>
2013-01-10 14:20:35 +0100

    remove a.out and update table.cpp

--- table.cpp
+++ table.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main ()
 {
 	int n1;
-	cout << "enter number";
-	cin >> n1;		
-	for (int i = 1; i <=10; i++ )
-		cout << n1*i <<  "  ";
+	cout << "enter number=";
+	cin >> n1;
+	for (int i = 1; i <=10; i++ ) 
+	cout <<  n1*i  <<  "   ";
 	return 0;
 
 
