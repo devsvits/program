e239ebbee73c19d1b3028d9c481d8d3b22fd75fc
root <root@Nagra.(none)>
2013-01-10 16:20:49 +0530

    adding divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout << "enter first number:";
+	cin>> n1;
+	cout << "enter second number:";
+	cin>> n2;
+	cout << n1/n2;
+	return 0;
+
+
+
+
+
+
+
+
+}
